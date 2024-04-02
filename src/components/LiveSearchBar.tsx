707ff7011d830a80e225d2f8536452f52193d316
Daniel Wilton <daniel.wilton@bjss.com>
2024-04-02 23:11:19 +0100

    Fix React rending options, update hydration

--- src/components/LiveSearchBar.tsx
+++ src/components/LiveSearchBar.tsx
@@ -1,9 +1,10 @@
 import { useStore } from "@nanostores/react";
 import { isKBarOpen, searchList, searchTerm } from "../stores/kbarStore";
+import { useEffect } from "react";
 
 export default function LiveSearchBar({ items }: { items: string[] }) {
   const $searchList = useStore(searchList);
-  let isFirstRun = true;
+  const $searchTerm = useStore(searchTerm);
 
   const handleInputChange = (event: { target: { value: any } }) => {
     const value = event.target.value;
@@ -12,36 +13,34 @@ export default function LiveSearchBar({ items }: { items: string[] }) {
     if (value) {
       const matches = items.filter((item) => item.includes(value));
       searchList.set(matches);
-    } else {
-      searchList.set(items);
     }
-
-    console.log(searchList.get());
   };
 
   isKBarOpen.subscribe((open: boolean) => {
     if (open) {
-      searchList.set(items);
-    } else {
-      if (!isFirstRun) {
-        searchList.set([]);
-      }
+      const inputElement = document.getElementById("kbarInput");
+      inputElement?.focus();
     }
-    isFirstRun = false;
   });
+
+  useEffect(() => {
+    if ($searchTerm == "") {
+      searchList.set(items);
+    }
+  }, [$searchTerm]);
 
   return (
     <>
       <div className="relative w-full pointer-events-auto h-fit">
         <input
+          id="kbarInput"
           type="text"
           className={
-            "flex items-center justify-end w-full px-4 py-6 text-sm border-t border-gray-200 rounded-lg ps-12 gap-x-2 focus:border-purple-700 focus:ring-purple-700 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-purple-600" +
-            ""
+            "flex items-center justify-end w-full px-4 py-6 text-sm border-t border-gray-200 rounded-lg ps-12 gap-x-2 focus:border-purple-700 focus:ring-purple-700 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-purple-600"
           }
-          autoFocus
           placeholder="This is placeholder"
           onChange={handleInputChange}
+          value={$searchTerm}
         />
         <div className="absolute inset-y-0 flex items-center text-gray-400 pointer-events-none dark:text-gray-500 start-0 ps-4 peer-disabled:opacity-50 peer-disabled:pointer-events-none">
           <svg
